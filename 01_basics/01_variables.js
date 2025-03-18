74ff8022993d6f36b3cfb374750af2ac2d2b8460
akashjirapure <akash.v.jirapure@gmail.com>
2025-03-18 18:21:17 +0530

    add new file 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const studentId = 123; // cant change value of const variable
+let studentName = "Akash Jirapure"
+var studentCity = "Amravati"
+studentState = "Maharashtra" // this is not good practice for declare variable
+
+// studentId = 355
+console.log(studentId)
+studentName = "Jay"
+ console.log(studentName);
+ 
